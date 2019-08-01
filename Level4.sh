e67bb5de2368e4349b515f15f25c1bd365e4d766
blueice123 <blueice_123@naver.com>
2019-08-01 08:03:06 +0000

    update level4

--- Level4.sh
+++ Level4.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# 필수 패키지 설치
+sudo yum update -y  >& /dev/null
+sudo yum install -y php-pecl-memcached >& /dev/null
+
 echo "Please enter Memcache endpoint(ex : megazone-hands-on.ndzwq5.cfg.apn2.cache.amazonaws.com:11211)"
 read memcache_endpoint
 
